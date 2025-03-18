9b0c5363d0a438af4b447d594f7af95647d86f48
Youcef Merzoug <merzoug.youcef@gmail.com>
2025-03-18 11:34:42 +0000

    fix stylecop

--- build/Configuration.cs
+++ build/Configuration.cs
@@ -1,12 +1,26 @@
 using System.ComponentModel;
-using Nuke.Common.Tooling;
 
+/// <summary>
+/// Build configuration
+/// </summary>
 [TypeConverter(typeof(TypeConverter<Configuration>))]
 public class Configuration : Enumeration
 {
+    /// <summary>
+    /// Debug configuration
+    /// </summary>
     public static Configuration Debug = new() { Value = nameof(Debug) };
+    
+    /// <summary>
+    /// Release configuration
+    /// </summary>
     public static Configuration Release = new() { Value = nameof(Release) };
 
+    /// <summary>
+    /// Implicit conversion from Configuration to string
+    /// </summary>
+    /// <param name="configuration">The configuration</param>
+    /// <returns></returns>
     public static implicit operator string(Configuration configuration)
     {
         return configuration.Value;
